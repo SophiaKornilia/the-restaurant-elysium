f6b5efd349673765c72808192e529f305ccbbb8c
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-02-09 14:31:45 +0100

    working with navigation

---
+++ src/Rounter.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Booking } from "./pages/Booking";
+import { Contact } from "./pages/Contact";
+import { Menu } from "./pages/Menu";
+import { AdminPage } from "./pages/AdminPage";
+import { NotFound } from "./pages/NotFound";
+import { Layout } from "./pages/Layout";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        errorElement: <NotFound />, 
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+                index: true,
+            },
+            {
+                path: '/menu',
+                element: <Menu />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            },
+            {
+                path: '/booking',
+                element: <Booking />,
+            }
+        ]
+    },
+    {
+        path: '/admin_page',
+        element: <AdminPage />
+    }
+])
