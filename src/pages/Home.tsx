4dc9d4279dadad03c8f308772ff68db1359e077a
Frida Alstergren <frida@alstergren.com>
2024-02-09 23:50:02 +0100

    Börjat på Innehållet i Home, WIP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,21 @@
-
 export const Home = () => {
-    return (
-        <>
-        <h1>Home</h1>
-        </>
-    ) 
-}
+  return (
+    <>
+      <div id="hero">
+        <img src="src/images/homePageHeroRotated.png" alt="Pasta on a fork" />
+      </div>
+      <div id="text-container">
+        <h1>E L Y S I U M</h1>
+        <h5>
+          At Elysium, we believe in creating a culinary journey that transcends
+          borders. Our restaurant is a celebration of diverse flavors, bringing
+          together the best elements from different cuisines to create a unique
+          and unforgettable dining experience. From the vibrant streets of Asia
+          to the rustic charm of Mediterranean kitchens, every dish at
+          Elysium is a masterpiece crafted with passion and innovation.
+        </h5>
+      </div>
+      <div></div>
+    </>
+  );
+};
