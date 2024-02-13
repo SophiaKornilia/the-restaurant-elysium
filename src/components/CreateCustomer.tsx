b99daa1e585fe0690485c2d1ccf810ee664394ee
Frida Alstergren <frida@alstergren.com>
2024-02-13 17:16:06 +0100

    Working together with the booking system and the API

---
+++ src/components/CreateCustomer.tsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useState, ChangeEvent } from "react";
+
+// 65c9d9502f64dba9babc81d6
+
+export const ApiComponent = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  var customerData = {
+    name: firstName,
+    lastname: lastName,
+    email: email,
+    phone: phone,
+  };
+
+  console.log(customerData);
+
+  const handleClick = async () => {
+    const response = await axios.post(
+      "https://school-restaurant-api.azurewebsites.net/customer/create",
+      customerData
+    );
+    console.log(response.data);
+
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPhone("");
+  };
+
+  return (
+    <>
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Firstname"
+            value={firstName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFirstName(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Lastname"
+            value={lastName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setLastName(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Phone"
+            value={phone}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setPhone(e.target.value);
+            }}
+          />
+          <button onClick={handleClick}>Create</button>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,15 +1,22 @@
 import axios from "axios";
-import { ChangeEvent, SetStateAction, useState } from "react";
+import { ChangeEvent, FormEvent, MouseEvent, SetStateAction, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 import { IBooking } from "../models/IBooking";
+import { Button, Modal } from "react-bootstrap";
+import { CreateCustomer } from "../components/CreateCutomer";
 
 export const Booking = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [people, setPeople] = useState<number>(1);
+  let [selectedTime, setSelSelectedTime] = useState('')
 
   const [bookings, setBookings] = useState<IBooking[]>();
+  const [counter1, setCounter1] = useState(1); // varför går det bara när vi börjar på 1? <----
+  const [counter2, setCounter2] = useState(1);
+
+  const [display, setDisplay] = useState(true);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPeople(parseInt(e.target.value));
@@ -23,25 +30,90 @@ export const Booking = () => {
     setBookings(response.data);
   };
 
-  const handleClick = () => {
-   
+  /**
+   * Användare väljer datum och antal gäster.
+   * då körs en get på booking
+   * bookingstate mappas igenom och letar efter samma datum och tiderna.
+   * varje gång den hittar en 18 tid sätts en counder till +1 samma med 21 mwen annan counter
+   * när den mappat alla tider checkar coundern av om den är < eller > än 6.
+   * Är den mindre visas två knappar som aktiva annars inaktiva och en alter med fullbokat syns.
+   */
+
+
+  /*
+    skapa en bokning
+    utifrån bokningen ta dess värde och skapa kunden.
+    När man trycker på "create" så skapas först en bokning och sedan en kund i samma handleClick
+    kunden ser sedan att boknigen är slutförd och dess id för att kunna avboka och ändra. 
+  
+  **/
+
+  const handleClick = (e: FormEvent) => {
+    e.preventDefault();
+
     bookings?.map((aBooking) => {
       console.log(aBooking.date);
-      const formattedSelectedDate = selectedDate?.toISOString().slice(0, 10);
-      if (JSON.stringify(aBooking.date) === JSON.stringify(formattedSelectedDate)) {
-        console.log("Det fungerar");
-      } else {
-        console.log("Nope de fungerar inte");
-        
+      const formattedSelectedDate = selectedDate?.toISOString().slice(0, 10); //också kolla tiderna
+      if (
+        JSON.stringify(aBooking.date) === JSON.stringify(formattedSelectedDate)
+      ) {
+        if (aBooking.time === "18:00") {
+          setCounter1(counter1 + 1); // uppdateras en efter <---
+          console.log(counter1);
+        } else if (aBooking.time === "21:00") {
+          setCounter1(counter1 + 1);
+          console.log(counter2);
+        }
+      } else if (counter1 >= 6 && counter2 >= 6) {
+        alert("Its fully booked, try another day!");
       }
     });
+
+    if (people) {
+      setDisplay(!display);
+    }
+
+    setCounter1(1);
+    setCounter2(1);
     return <div></div>;
   };
 
-  if (!bookings) {
-    searchBooking();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleClickTimeBtn1 = () => {
+    setShow(true);
+    selectedTime = "18:00";
+    console.log(selectedTime)
+
+    return <></>;
+  };
+
+  const CreateBooking = async () => {
+
+    var bookingData = {
+      
+      restaurantId: "623b85d54396b96c57bde7c3",
+      date: selectedDate,
+      time: selectedTime, 
+      numberOfGuests: people
+      
+    };
+
+    const response = await axios.post(
+      "https://school-restaurant-api.azurewebsites.net/booking/create",
+      bookingData
+    );
+    console.log(response.data);
+
+
+    return ( 
+    <>
+    </>
+    ) 
   }
 
+  
   return (
     <div>
       {/* <BookingGet booking={} /> */}
@@ -51,7 +123,7 @@ export const Booking = () => {
           <p>your experience</p>
         </div>
         <div>
-          <form id="form-container">
+          <form id="form-container" onSubmit={handleClick}>
             <input
               type="number"
               min={1}
@@ -67,18 +139,38 @@ export const Booking = () => {
               onChange={(date: SetStateAction<Date | null>) =>
                 setSelectedDate(date)
               }
-              dateFormat="yyyy-MM-DD"
+              dateFormat="yyyy-MM-dd"
               minDate={new Date()}
-              // showTimeSelect
-              // includeTimes={[
-              //   setHours(setMinutes(new Date(), 0), 18),
-              //   setHours(setMinutes(new Date(), 0), 21),
-              // ]}
             ></DatePicker>
             <br></br>
             <p>Click here to cancel a booking</p>
+            <button type="submit">Search available tables</button>
           </form>
-          <button onClick={handleClick}>Search available tables</button>
+          <div className={display ? "display" : ""}>
+            <div className="times">
+              <h4>Pick a time</h4>
+              <button className="time-btn" onClick={handleClickTimeBtn1}>
+                18:00
+              </button>
+              <button className="time-btn">21:00</button>
+            </div>
+          </div>
+          <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton>
+              <Modal.Title>Personal information</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <CreateCustomer />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleCreateBooking}>
+                Confirm
+              </Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
     </div>
