a65b72d4f9914c43b759662d0b7f8357c7f69c59
Frida Alstergren <frida@alstergren.com>
2024-02-21 12:43:49 +0100

    Css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,29 +3,38 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <>
-
       <div className="hero-image">
         <div className="hero-text">
           <h1>E L Y S I U M</h1>
-          <h5>
-            At Elysium, we believe in creating a culinary journey that
-            transcends borders. Our restaurant is a celebration of diverse
-            flavors, bringing together the best elements from different cuisines
-            to create a unique and unforgettable dining experience. From the
-            vibrant streets of Asia to the rustic charm of Mediterranean
-            kitchens, every dish at Elysium is a masterpiece crafted with
-            passion and innovation.
-          </h5>
+          <h6>
+            New flavours in the heart of Gothenburg.
+            <br />
+            Reserve a table and enjoy the experience.
+          </h6>
         </div>
       </div>
       <div className="background-container">
         <div className="image-container">
-        <Link to="/booking">
-          <button className="book-btn">Book a table</button>
-          </Link>
-          <Link to="/menu">
-          <button className="menu-btn"> Menu</button>
-          </Link>
+          <div className="btn-container">
+            <Link to="/booking">
+              <button className="book-btn">Book a table</button>
+            </Link>
+            <Link to="/menu">
+              <button className="menu-btn"> Menu</button>
+            </Link>
+          </div>
+          <div className="text-container">
+            <h6>
+              At Elysium, we believe in creating a culinary journey that
+              transcends borders. Our restaurant is a celebration of diverse
+              flavors, bringing together the best elements from different
+              cuisines to create a unique and unforgettable dining experience.
+              <br />
+              <br />From the vibrant streets of Asia to the rustic charm of
+              Mediterranean kitchens, every dish at Elysium is a masterpiece
+              crafted with passion and innovation.
+            </h6>
+          </div>
         </div>
       </div>
     </>
